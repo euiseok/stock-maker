b826c6b46117a60aa257fd5b8ebc276e729b4eef
euiseok.yang <euiseok.yang@gmail.com>
2012-07-14 09:50:11 +0000

    Hibernate

---
+++ stock-maker/Neutrino/src/main/java/com/neutrino/dao/impl/MemberDaoHibernate.java
@@ -0,0 +1,30 @@
+package com.neutrino.dao.impl;
+
+
+
+import org.apache.log4j.Logger;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import com.neutrino.dao.MemberDao;
+import com.neutrino.model.Member;
+
+public class MemberDaoHibernate extends HibernateDaoSupport implements MemberDao{
+
+	private static Logger log = Logger.getLogger(MemberDaoHibernate.class);
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public Member findById(final String email) {
+		return (Member)getHibernateTemplate().execute(new HibernateCallback(){
+			@Override
+			public Object doInHibernate(Session session){
+				Query query = getSession().getNamedQuery("com.neutrino.sql.memberByEmail");
+				query.setString("email",email);
+				return (Member)query.uniqueResult();
+			}});
+	}
+	
+
+}
